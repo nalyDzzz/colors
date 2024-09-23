6f9b5b6776643a2c7eeec03b9ca5f3fae4272af0
Dylan <dylanmarin2018@gmail.com>
2024-09-23 16:34:45 -0600

    boilerplate ready

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="container lg mx-auto">
+        <h1 className="text-4xl">Home Page</h1>
+      </div>
+    </main>
+  );
+}
