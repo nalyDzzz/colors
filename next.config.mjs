f456e80cc4f321856f434f4a118f069e78e20086
Dylan <dylanmarin2018@gmail.com>
2024-09-28 15:58:21 -0600

    remove tree shaking

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    optimizePackageImports: ['@mantine/hooks'],
-  },
   output: 'standalone',
   reactStrictMode: true,
 };
